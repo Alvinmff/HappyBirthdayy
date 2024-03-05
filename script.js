ba75a4d063db4257fdcf2a39593a6b11fbab2073
Alvinmff <161628528+Alvinmff@users.noreply.github.com>
2024-03-06 00:17:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.open').addEventListener('click', () => {
+  window.location.href = 'https://secreto.site/id/agsvg7'; 
+  window.location.replace = 'https://secreto.site/id/agsvg7';
+}
